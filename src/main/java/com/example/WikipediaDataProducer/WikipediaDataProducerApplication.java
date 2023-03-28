0fb0ad6ec3979b1ec5cd7b133399ce0bc2b8b708
Deepak Saini <deepak1307saini@gmail.com>
2023-03-28 12:59:25 +0530

    initialization

---
+++ src/main/java/com/example/WikipediaDataProducer/WikipediaDataProducerApplication.java
@@ -0,0 +1,13 @@
+package com.example.WikipediaDataProducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WikipediaDataProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WikipediaDataProducerApplication.class, args);
+	}
+
+}
